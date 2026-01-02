f2b4b5cf0fad53bca4f16f8be35e077d929525f0
madistic <forchegg72@gmail.com>
2026-01-03 02:47:46 +0530

    Ready Production

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/PMKSteel">
+  <BrowserRouter basename="/">
     <App />
   </BrowserRouter>
 );
